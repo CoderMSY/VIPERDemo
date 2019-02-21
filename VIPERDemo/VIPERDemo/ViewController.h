2d815279ec6009a2acf9e76ad785497be641b2d0
SimonMiao <2278046701@qq.com>
2019-02-21 14:46:16 +0800

    工程初始化

---
+++ VIPERDemo/VIPERDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VIPERDemo
+//
+//  Created by SimonMiao on 2019/2/21.
+//  Copyright © 2019 avatar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VIPERDemo/VIPERDemoTests/VIPERDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  VIPERDemoTests.m
+//  VIPERDemoTests
+//
+//  Created by SimonMiao on 2019/2/21.
+//  Copyright © 2019 avatar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VIPERDemoTests : XCTestCase
+
+@end
+
+@implementation VIPERDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
